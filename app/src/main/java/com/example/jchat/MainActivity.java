728749304e229f8aae67bafe485de0147df56f0d
Kousthubha <krishnakousthubha@gmail.com>
2020-01-02 15:44:52 +0530

    Firebase Login Added

--- app/src/main/java/com/example/jchat/MainActivity.java
+++ app/src/main/java/com/example/jchat/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.jchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +33,23 @@ public class MainActivity extends AppCompatActivity {
         String emailString = email.getText().toString();
         EditText password = (EditText)findViewById(R.id.jpassword);
         String passwordString = password.getText().toString();
-        if(emailString.equals("") && passwordString.equals(""))
-        {
-            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
-            Intent in = new Intent(this,ChatActivity.class);
-            Log.i("Error Check","Entered");
-            startActivity(in);
-        }
-        else
-        {
-            Toast.makeText(this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
-        }
+
+        mAuth.signInWithEmailAndPassword(emailString,passwordString)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            Intent in = new Intent(MainActivity.this,ChatActivity.class);
+                            startActivity(in);
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
